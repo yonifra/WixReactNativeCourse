7f411af7311ad0acc2513c8558002074885e9823
Jonathan Fraimorice <jonathanfr@wix.com>
2019-09-23 14:04:55 +0300

    added basic navigation between two pages

--- src/screens/PostsList.js
+++ src/screens/PostsList.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {Navigation} from 'react-native-navigation';
 
 class PostsList extends Component {
+  pushViewPostScreen = () => {
+    Navigation.push(this.props.componentId, {
+      component: {
+        name: 'ViewPost',
+        passProps: {
+          somePropToPass: 'Some props that we are passing',
+        },
+        options: {
+          topBar: {
+            title: {
+              text: 'View Post',
+            },
+          },
+        },
+      },
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>PostsList Screen</Text>
+        <Text style={styles.text} onPress={this.pushViewPostScreen}>
+          PostsList Screen
+        </Text>
       </View>
     );
   }
